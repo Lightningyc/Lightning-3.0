bbba3e9983130b8066571dff9c7d32379c149a46
Lightningyc <2034829801@qq.com>
2022-10-29 11:45:59 +0800

    循环语句

---
+++ 闪电的代码/循环语句/循环语句/循环语句.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 0;
+	while (a<= 365)//a或者line只是定义了变量的名称，//
+		          //实际上就是行数。//
+	{
+		printf("学习:%d\n",a);
+		a++;
+	}
+	if(a > 365)
+	{
+		printf("上岸\n");
+	}
+	return 0;
+}
