a6ab06c09b450eac3ce7079bd01cb97ebf333408
Lightningyc <2034829801@qq.com>
2022-10-29 14:19:56 +0800

    函数

---
+++ 闪电的代码/函数/函数/函数.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int Add(int x, int y)//函数模块化
+{
+	int z = x * y;
+	return z;
+}
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	printf("输入两个操作数:");
+	scanf("%d %d", &num1, &num2);
+	int sum = Add(num1, num2);//Add只是一个自定义的函数名
+	printf("sum=%d\n", sum);
+	return 0;
+}
