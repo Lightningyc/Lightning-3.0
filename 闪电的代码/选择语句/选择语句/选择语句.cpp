f28c9362be441423128ae25f48ddd6dd55b1158f
Lightningyc <2034829801@qq.com>
2022-10-29 11:23:30 +0800

    添加代码

---
+++ 闪电的代码/选择语句/选择语句/选择语句.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1//解决scanf报错的问题
+#include <stdio.h>
+int main()
+{
+	int input = 0;//输入的值
+	    printf("考上好大学");
+	    printf("那要好好学习吗(1/0)? :");
+	    scanf("%d", &input);
+	if (input == 1)
+		printf("上岸985，211\n");
+	else
+		printf("复读\n");
+	return 0;
+}
